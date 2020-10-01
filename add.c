618ba0b377cf1976552127a30640616e0b7f5032
devmalik7 <72170432+devmalik7@users.noreply.github.com>
2020-10-02 01:10:14 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    printf("enter the value of A");
+    scanf ("%d", &a);
+    printf ("enter the valuee of B");
+    scanf("%d", &b);
+    int c= a+b;
+    printf("he sum of two numbers is %d" , c);
+    return 0;
+   
+}
