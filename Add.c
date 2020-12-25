95c6f2c990b3b11d145bed86949e34390b86c290
aakashgoel2002 <65633502+aakashgoel2002@users.noreply.github.com>
2020-12-25 23:38:13 +0530

    Add.c

---
+++ Add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the value of A: ");
+    scanf("%d",&a);
+    printf("Enter the value of B: ");
+    scanf("%d",&b);
+    int c=a+b;
+    printf("The sum of two numbers is %d" ,c);
+    return 0;
+}
