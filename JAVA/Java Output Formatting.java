05c91072dfa1f0072dd84979e2c6b515a17fba6e
devmalik7 <72170432+devmalik7@users.noreply.github.com>
2021-01-22 23:55:13 +0530

    Update Java Output Formatting.java

--- JAVA/Java Output Formatting.java
+++ JAVA/Java Output Formatting.java
@@ -4,13 +4,13 @@ public class Solution {
 
     public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
-            System.out.println("================================");
+            System.out.println("====================");
             for(int i=0;i<3;i++){
                 String s1=sc.next();
                 int x=sc.nextInt();
                 //Complete this line
             }
-            System.out.println("================================");
+            System.out.println("=====================");
 
     }
 }
