69300530f727b0b95e02e0663c157ce354202586
devmalik7 <72170432+devmalik7@users.noreply.github.com>
2021-01-24 23:47:57 +0530

    Update total number of characters in a string.java

--- JAVA/String/total number of characters in a string.java
+++ JAVA/String/total number of characters in a string.java
@@ -8,9 +8,7 @@
             for(int i = 0; i < string.length(); i++) {    
                 if(string.charAt(i) != ' ')    
                     count++;    
-            }    
-                
-            //Displays the total number of characters present in the given string    
+            }  
             System.out.println("Total number of characters in a string: " + count);    
         }    
     }     
