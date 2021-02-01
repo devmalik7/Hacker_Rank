330a5ee9826746f853eea2df8c8ae461380184e7
devmalik7 <devm333666@gmail.com>
2021-02-01 23:16:46 +0530

    Update Java End-of-file.java

--- JAVA/Java End-of-file.java
+++ JAVA/Java End-of-file.java
@@ -4,11 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
-
-
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-    
+public class Solution {    
  public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int line_num = 0;
